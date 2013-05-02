5e585d1d6090fb3078e4f09df2f1b331122e2d71
cesar <cesar@pissedoffadmins.com>
2013-05-02 19:41:59 -0400

    Updated to add read function.

--- rfc-index.sh
+++ rfc-index.sh
@@ -1,49 +1,70 @@
 #!/bin/bash
 
+# vim:set ts=2 sw=4 noexpandtab:
 # <cesar@pissedoffadmins.com> 2013
 
-# Copying and distribution of this file, with or without modification,
-# are permitted in any medium without royalty provided the copyright
-# notice and this notice are preserved.  This file is offered as-is,
-# without any warranty.
-
-# TODO:
-#
-
+set -e
 NAME=$(basename $0)
 
 version()
 {
-    local VER="0.1"
-    printf -- "%s\n" "${NAME} version ${VER}"
-    printf -- "%s\n" "<cesar@pissedoffadmins.com> 2013"
-    printf -- "%s\n"
+  local VER="0.1"
+  printf -- "%s\n" "${NAME} version ${VER}"
+  printf -- "%s\n" "<cesar@pissedoffadmins.com> 2013"
+  printf -- "%s\n"
 }
 
 license()
 {
   cat <<EOL
-Copying and distribution of this file, with or without modification,
-are permitted in any medium without royalty provided the copyright
-notice and this notice are preserved.  This file is offered as-is,
-without any warranty.
+Use and modify as needed.
 EOL
 }
 
 usage()
 {
   printf -- "%s\n"
-  printf -- "%s\n" "Usage: ${NAME} <####>"
-  printf -- "%s\n" "Examples:"
-  printf -- "%s\n" "  ${NAME} 1        # displays RFC 0001 index"
+  printf -- "%s\n" "Usage: ${NAME} <name|read> <####>"
+  printf -- "%s\n" "Usage examples:"
+  printf -- "%s\n" "  ${NAME} name 1        # displays RFC 0001 index"
+  printf -- "%s\n" "  ${NAME} read 1443     # read RFC 1443"
 }
 
-[ ${1} -ne 0 -o ${1} -eq 0 2>/dev/null ] || { version; license; usage; exit 1; }
-FN=`printf "%04d" ${1} | xargs`
+## check if $# -eq 2 && $2 is an integer
+[ $# -eq 2 ] || { version; license; usage; exit 1; }
+[ ${2} -ne 0 -o ${2} -eq 0 2>/dev/null ] || { version; license; usage; exit 1; }
 
-case "${FN}" in
-  [0-9]|[[0-9][0-9]|[0-9][0-9][0-9]|[0-9][0-9][0-9][0-9])
-    curl -s http://www.rfc-editor.org/rfc/rfc-index.txt | \
-    awk '/^'${FN}'/ {do_print=1} do_print==1 {print} NF==0 {do_print=0}';;
-  *) printf -- "Error: unknown parameter '%s'\n" "$1"; usage; exit 1;;
+## prepend zeros to make id number <####>
+FN=`printf "%04d" ${2} | xargs`
+
+## temp file and trap statement - trap for clean end
+TMP_FILE=$(mktemp --tmpdir rfc.$$.XXXXXXXXXX)
+trap "rm -rf ${TMP_FILE}" 0 1 2 3 15
+
+## editor / viewer settings
+[ -x $(which xterm 2>/dev/null) ] && ED="$(which xterm 2>/dev/null)" \
+  || [ -x $(which mrxvt 2>/dev/null) ] && ED="$(which mrxvt 2>/dev/null)" \
+  || [ -x $(which urxvt 2>/dev/null) ] && ED="$(which urxvt 2>/dev/null)"
+ED_SETTINGS="-fg green -bg black -bd green -g 72x59 -T"
+ED_TITLE="${NAME} - rfc${FN}.txt"
+
+case "${1}" in
+  'name'|'index')
+    case "${FN}" in
+      [0-9]|[[0-9][0-9]|[0-9][0-9][0-9]|[0-9][0-9][0-9][0-9])
+      curl -s http://www.rfc-editor.org/rfc/rfc-index.txt | \
+        awk '/^'${FN}'/ {do_print=1} do_print==1 {print} NF==0 {do_print=0}';;
+    *) printf -- "Error: unknown parameter '%s'\n" "$2"; usage; exit 1;;
+    esac
+  ;;
+
+  'read'|'show')
+    printf -- "%s" "Grabbing ${FN}"
+    wget -O- -q http://www.rfc-editor.org/rfc/rfc${FN}.txt | \
+      awk '{line++; print}; /\f/ {for (i=line; i<=58; i++) print ""; line=0}' | \
+      sed '/\f/d' > "${TMP_FILE}"
+    printf -- "\ndone grabbing\n"
+    ${ED} ${ED_SETTINGS} "${ED_TITLE}" -e ${PAGER} "${TMP_FILE}"
+  ;;
+  *) printf -- "stuffs\n"; usage; exit 1;;
 esac
