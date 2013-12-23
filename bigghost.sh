bc4c59551ff7ae2a70f892e88c07a2655d31e878
cbodden <cesar@pissedoffadmins.com>
2013-12-22 21:58:42 -0500

    Added more content

--- bigghost.sh
+++ bigghost.sh
@@ -10,17 +10,18 @@ GRN=$(tput setaf 2); YLW=$(tput setaf 3); RED=$(tput setaf 1); CLR=$(tput sgr0)
 PRE=("" "Muthafuckin wise n powerful" "The world famous" "The imperial"
   "That muthafucka wit two iron midgets for hands" "The almighty"
   "The grand emperor" "The grand royal" "The illustrious" "The magnificent"
-  "The super supreme")
+  "The super supreme" "The unfuckwittable" "The majestic")
 PRE_INDEX=$(( $RANDOM % ${#PRE[*]} ))
 PRE_W_CNT=$(( ${#PRE[$PRE_INDEX]} ))
 PRE_OUT=${GRN}${PRE[$PRE_INDEX]}
 
-MID1=("Broccoli" "Caviar" "Cocaine" "Divine" "Galaxy" "Hands_of_Zeus" "Phantom"
-  "Shampoo" "Spartacus" "Swole_Ya_Eye" "Thor" "Volcano" "Watch_Ya_Mouf")
+MID1=("Broccoli" "Caviar" "Cocaine" "Diamond" "Divine" "Galaxy" "Hands_of_Zeus"
+  "Phantom" "Shampoo" "Spartacus" "Swole_Ya_Eye" "Thor" "Volcano"
+  "Watch_Ya_Mouf")
 MID1_INDEX=$(( $RANDOM % ${#MID1[*]} ))
 MID1_W_CNT=$(( ${#MID1[$MID1_INDEX]} ))
 if [ `echo ${MID1[$MID1_INDEX]} | cut -d_ -f1 -s | wc -l` -lt 1 ]; then
-  MID2=("Bundles" "Tusks" "Biceps" "Snowcones" "Knuckles" "Raviolis"
+  MID2=("Bundles" "Tusks" "Biceps" "Ligaments" "Snowcones" "Knuckles" "Raviolis"
     "Bracelets" "Deluxe" "Molecules" "Hands")
   MID2_INDEX=$(( $RANDOM % ${#MID2[*]} ))
   MID_W_CNT=$(( ${#MID1[$MID1_INDEX]} + ${#MID2[$MID2_INDEX]} ))
@@ -32,7 +33,7 @@ fi
 
 POST=("" "in the flesh" "the magnificent" "the panty melter" "the great"
   "the wallabee champ" "via amazin wizardry n shit"
-  "the Stapleton gladiator namsayin")
+  "the Stapleton gladiator namsayin" "n all that" "namsayin")
 POST_INDEX=$(( $RANDOM % ${#POST[*]} ))
 POST_W_CNT=$(( ${#POST[$POST_INDEX]} ))
 POST_OUT=${YLW}${POST[$POST_INDEX]}${CLR}
