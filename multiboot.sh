d6dc7596101440d87597ad1db7542a2d76762093
cbodden <cesar@knewton.com>
2014-04-16 14:30:31 -0400

    cleaned up the function names to make sense

--- multiboot.sh
+++ multiboot.sh
@@ -56,8 +56,8 @@ function disk_detect()
 
 function disk_action()
 {
+## do not touch this section or you break the fdisk function!!!
 dd if=/dev/zero of=${USBSTICK} bs=512 count=62
-## do not touch the next couple of lines or you break fdisk!!!
 fdisk ${USBSTICK} <<EOF
 d
 
@@ -80,7 +80,7 @@ EOF
   mkfs.vfat ${USBSTICK}1
 }
 
-function grub2()
+function disk_grub2()
 {
   ## begin grub2 stuff
   mkdir /${USBTMPDIR}
@@ -96,7 +96,7 @@ function cleanup()
   rm /${USBTMPDIR} -rf
 }
 
-function grub_header()
+function install_grub_header()
 {
 echo "set timeout=300
 set default=0
@@ -105,7 +105,7 @@ set menu_color_highlight=white/green
 " >> ${GRUBCONF}
 }
 
-function debian_amd64()
+function install_debian_amd64()
 {
 DL_ADDY=""
 IMAGE=""
@@ -120,7 +120,7 @@ echo "menuentry \"Debian netinst 7.4.0 amd64\" {
 " >> ${GRUBCONF}
 }
 
-function gentoo_amd64()
+function install_gentoo_amd64()
 {
 DL_ADDY="http://distfiles.gentoo.org/releases/amd64/autobuilds/20140403/"
 IMAGE="install-amd64-minimal-20140403.iso"
@@ -138,7 +138,7 @@ echo "menuentry \"Gentoo minimal 20140403 amd64\" {
 wget ${DL_ADDY}${IMAGE}  --directory-prefix=/${USBTMPDIR}/iso/
 }
 
-function gentoo_i386()
+function install_gentoo_i386()
 {
 DL_ADDY="http://distfiles.gentoo.org/releases/x86/autobuilds/20140415/"
 IMAGE="install-x86-minimal-20140415.iso"
@@ -156,7 +156,7 @@ echo "menuentry \"Gentoo minimal 20140415 i386\" {
 wget ${DL_ADDY}${IMAGE}  --directory-prefix=/${USBTMPDIR}/iso/
 }
 
-function kali_amd64()
+function install_kali_amd64()
 {
 DL_ADDY="http://cdimage.kali.org/kali-latest/amd64/"
 IMAGE="kali-linux-1.0.6-amd64.iso"
@@ -174,7 +174,7 @@ echo "menuentry \"Kali Linux 1.0.6 amd64\" {
 wget ${DL_ADDY}${IMAGE}  --directory-prefix=/${USBTMPDIR}/iso/
 }
 
-function netbsd_i386()
+function install_netbsd_i386()
 {
 echo "menuentry \"NetBSD 6.1.3 i386\" {
   set isofile="/iso/NetBSD-6.1.3-i386.iso"
@@ -186,7 +186,7 @@ echo "menuentry \"NetBSD 6.1.3 i386\" {
 " >> ${GRUBCONF}
 }
 
-function openbsd54_amd64()
+function install_openbsd54_amd64()
 {
 DL_ADDY="http://ftp.openbsd.org/pub/OpenBSD/5.4/amd64/"
 WGET_OPTIONS="-r -l 1 -nd -e robots=off --reject index.html"
@@ -201,7 +201,7 @@ echo "menuentry \"OpenBSD 5.4 amd64\" {
 wget -nd -r ${DL_ADDY} ${WGET_OPTIONS} ${WGET_PATH}
 }
 
-function openbsd54_i386()
+function install_openbsd54_i386()
 {
 DL_ADDY="http://ftp.openbsd.org/pub/OpenBSD/5.4/i386/"
 WGET_OPTIONS="-r -l 1 -nd -e robots=off --reject index.html"
@@ -216,7 +216,7 @@ echo "menuentry \"OpenBSD 5.4 i386\" {
 wget -nd -r ${DL_ADDY} ${WGET_OPTIONS} ${WGET_PATH}
 }
 
-function tails_i386()
+function install_tails_i386()
 {
 DL_ADDY="http://dl.amnesia.boum.org/tails/stable/tails-i386-0.23/"
 IMAGE="tails-i386-0.23.iso"
@@ -235,7 +235,7 @@ echo "menuentry \"Tails 0.23 i386\" {
 wget ${DL_ADDY}${IMAGE}  --directory-prefix=/${USBTMPDIR}/iso/
 }
 
-function ubuntu12s_amd64()
+function install_ubuntu12s_amd64()
 {
 DL_ADDY="http://releases.ubuntu.com/12.04.4/"
 IMAGE="ubuntu-12.04.4-server-amd64.iso"
@@ -251,7 +251,7 @@ echo "menuentry \"Ubuntu 12.04 server amd64\" {
 wget ${DL_ADDY}${IMAGE}  --directory-prefix=/${USBTMPDIR}/iso/
 }
 
-function ubuntu13d_amd64()
+function install_ubuntu13d_amd64()
 {
 DL_ADDY="http://releases.ubuntu.com/13.10/"
 IMAGE="ubuntu-13.10-desktop-amd64.iso"
@@ -267,7 +267,7 @@ echo "menuentry \"Ubuntu 13.10 desktop amd64\" {
 wget ${DL_ADDY}${IMAGE}  --directory-prefix=/${USBTMPDIR}/iso/
 }
 
-function ubuntu13d_i386()
+function install_ubuntu13d_i386()
 {
 DL_ADDY="http://releases.ubuntu.com/13.10/"
 IMAGE="ubuntu-13.10-desktop-i386.iso"
@@ -287,17 +287,17 @@ wget ${DL_ADDY}${IMAGE}  --directory-prefix=/${USBTMPDIR}/iso/
 
 disk_detect
 disk_action
-grub2
-grub_header
-debian_amd64
-gentoo_amd64
-gentoo_i386
-kali_amd64
-netbsd_i386
-openbsd54_amd64
-openbsd54_i386
-tails_i386
-ubuntu12s_amd64
-ubuntu13d_amd64
-ubuntu13d_i386
+disk_grub2
+install_grub_header
+install_debian_amd64
+install_gentoo_amd64
+install_gentoo_i386
+install_kali_amd64
+install_netbsd_i386
+install_openbsd54_amd64
+install_openbsd54_i386
+install_tails_i386
+install_ubuntu12s_amd64
+install_ubuntu13d_amd64
+install_ubuntu13d_i386
 cleanup
