f587d1807a35ae8571d217714e63c26f489a630f
cesar <cesar@pissedoffadmins.com>
2014-04-17 23:23:03 -0400

    refactored more lines

--- multiboot.sh
+++ multiboot.sh
@@ -46,16 +46,15 @@ function disk_detect()
         { echo -e "${DRV} is used by:\n$(df | grep "${DRV}")"; exit 1; } ||
         { USBSTICK="${DRV}"; } ;;
     esac
-  DRV_CLEAN=$(echo "${USBSTICK}" | cut -d"/" -f3)
-  # UUID=`ls -al /dev/disk/by-uuid/ | grep ${DRV_CLEAN}1 | awk '{print $9}'`
-  break
+    DRV_CLEAN=$(echo "${USBSTICK}" | cut -d"/" -f3)
+    break
   done
 }
 
 function disk_action()
 {
-## do not touch this section or you break the fdisk function!!!
-dd if=/dev/zero of=${USBSTICK} bs=512 count=62
+  ## do not touch this section or you break the fdisk function!!!
+  dd if=/dev/zero of=${USBSTICK} bs=512 count=62
 fdisk ${USBSTICK} <<EOF
 d
 
@@ -107,9 +106,9 @@ set menu_color_highlight=white/green
 
 function install_debian_amd64()
 {
-VER="7.4.0"
-DL_ADDY="http://cdimage.debian.org/debian-cd/${VER}/amd64/iso-cd/"
-IMAGE="debian-${VER}-amd64-netinst.iso"
+  VER="7.4.0"
+  DL_ADDY="http://cdimage.debian.org/debian-cd/${VER}/amd64/iso-cd/"
+  IMAGE="debian-${VER}-amd64-netinst.iso"
 
 echo "menuentry \"Debian netinst ${VER} amd64\" {
   set isofile=\"/iso/${IMAGE}\"
@@ -119,19 +118,22 @@ echo "menuentry \"Debian netinst ${VER} amd64\" {
   initrd (loop)/install.amd/initrd.gz
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+  wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
 }
 
-function install_fedora_amd64()
+function install_fedora()
 {
-VER="20"
-DL_ADDY="download.fedoraproject.org/pub/fedora/linux/releases/${VER}/Live/x86_64/"
-IMAGE="Fedora-Live-Desktop-x86_64-${VER}-1.iso"
-FED_OPTS="--class fedora --class gnu-linux --class gnu --class os"
-
-echo "menuentry \"Fedora desktop ${VER} amd64\" ${FED_OPTS} {
+  VER=$1
+  shift 1
+  while [[ $# -gt 0 ]]; do
+    [[ "$1" == "i386" ]] && { 3VER="i386" ; 6VER="i686" ; } ||
+      { 3VER=$1 ; 6VER=$1 ; }
+    DL_ADDY="mirror.pnl.gov/fedora/linux/releases/${VER}/Live/${3VER}/"
+    IMAGE="Fedora-Live-Desktop-${6VER}-${VER}-1.iso"
+    FED_OPTS="--class fedora --class gnu-linux --class gnu --class os"
+echo "menuentry \"Fedora desktop ${VER} ${3VER}\" ${FED_OPTS} {
   insmod loopback
-  set isolabel=Fedora-Live-Desktop-x86_64-${VER}-1
+  set isolabel=Fedora-Live-Desktop-${6VER}-${VER}-1
   set isofile=\"/iso/${IMAGE}\"
   set bo1=\"iso-scan/filename=\$isofile\"
   set bo2=\"root=live:LABEL=\$isolabel ro rd.live.image quiet rhgb\"
@@ -141,36 +143,16 @@ echo "menuentry \"Fedora desktop ${VER} amd64\" ${FED_OPTS} {
   initrd /isolinux/initrd0.img
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
-}
-
-function install_fedora_i386()
-{
-VER="20"
-DL_ADDY="download.fedoraproject.org/pub/fedora/linux/releases/${VER}/Live/i386/"
-IMAGE="Fedora-Live-Desktop-i686-${VER}-1.iso"
-FED_OPTS="--class fedora --class gnu-linux --class gnu --class os"
-
-echo "menuentry \"Fedora desktop ${VER} i386\" ${FED_OPTS} {
-  insmod loopback
-  set isolabel=Fedora-Live-Desktop-i686-${VER}-1
-  set isofile=\"/iso/${IMAGE}\"
-  set bo1=\"iso-scan/filename=\$isofile\"
-  set bo2=\"root=live:LABEL=\$isolabel ro rd.live.image quiet rhgb\"
-  loopback loop (hd0,1)/\$isofile
-  set root=(loop)
-  linux /isolinux/vmlinuz0 \$bo1 \$bo2
-  initrd /isolinux/initrd0.img
-}
-" >> ${GRUBCONF}
- wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+    wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+    shift 1
+  done
 }
 
 function install_gentoo_amd64()
 {
-VER="20140403"
-DL_ADDY="http://distfiles.gentoo.org/releases/amd64/autobuilds/${VER}/"
-IMAGE="install-amd64-minimal-${VER}.iso"
+  VER="20140403"
+  DL_ADDY="http://distfiles.gentoo.org/releases/amd64/autobuilds/${VER}/"
+  IMAGE="install-amd64-minimal-${VER}.iso"
 
 echo "menuentry \"Gentoo minimal ${VER} amd64\" {
   set isofile=\"/iso/${IMAGE}\"
@@ -182,14 +164,14 @@ echo "menuentry \"Gentoo minimal ${VER} amd64\" {
   initrd (loop)/isolinux/gentoo.igz
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+  wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
 }
 
 function install_gentoo_i386()
 {
-VER="20140415"
-DL_ADDY="http://distfiles.gentoo.org/releases/x86/autobuilds/${VER}/"
-IMAGE="install-x86-minimal-${VER}.iso"
+  VER="20140415"
+  DL_ADDY="http://distfiles.gentoo.org/releases/x86/autobuilds/${VER}/"
+  IMAGE="install-x86-minimal-${VER}.iso"
 
 echo "menuentry \"Gentoo minimal ${VER} i386\" {
   set isofile=\"/iso/${IMAGE}\"
@@ -201,7 +183,7 @@ echo "menuentry \"Gentoo minimal ${VER} i386\" {
   initrd (loop)/isolinux/gentoo.igz
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+  wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
 }
 
 function install_kali()
@@ -263,7 +245,7 @@ function install_openbsd()
 echo "menuentry \"OpenBSD ${VER} ${1}\" {
   insmod ext2
   set root=(hd0,msdos1)
-  kopenbsd /5.4/${1}/bsd.rd
+  kopenbsd /${VER}/${1}/bsd.rd
 }
 " >> ${GRUBCONF}
     wget ${DL_ADDY} ${WGET_OPTIONS} ${WGET_PATH} || echo "OpenBSD dloaded"
@@ -273,8 +255,8 @@ echo "menuentry \"OpenBSD ${VER} ${1}\" {
 
 function install_tails_i386()
 {
-DL_ADDY="http://dl.amnesia.boum.org/tails/stable/tails-i386-0.23/"
-IMAGE="tails-i386-0.23.iso"
+  DL_ADDY="http://dl.amnesia.boum.org/tails/stable/tails-i386-0.23/"
+  IMAGE="tails-i386-0.23.iso"
 
 echo "menuentry \"Tails 0.23 i386\" {
   set isofile=\"/iso/${IMAGE}\"
@@ -285,7 +267,7 @@ echo "menuentry \"Tails 0.23 i386\" {
   initrd (loop)/live/initrd.img
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+  wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
 }
 
 function install_tails_i386_masq()
@@ -305,8 +287,8 @@ echo "menuentry \"Tails 0.23 i386 masquerade\" {
 
 function install_ubuntu12s_amd64()
 {
-DL_ADDY="http://releases.ubuntu.com/12.04.4/"
-IMAGE="ubuntu-12.04.4-server-amd64.iso"
+  DL_ADDY="http://releases.ubuntu.com/12.04.4/"
+  IMAGE="ubuntu-12.04.4-server-amd64.iso"
 
 echo "menuentry \"Ubuntu 12.04 server amd64\" {
   set isofile=\"/iso/${IMAGE}\"
@@ -316,13 +298,13 @@ echo "menuentry \"Ubuntu 12.04 server amd64\" {
   initrd (loop)/casper/initrd.lz
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+  wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
 }
 
 function install_ubuntu13d_amd64()
 {
-DL_ADDY="http://releases.ubuntu.com/13.10/"
-IMAGE="ubuntu-13.10-desktop-amd64.iso"
+  DL_ADDY="http://releases.ubuntu.com/13.10/"
+  IMAGE="ubuntu-13.10-desktop-amd64.iso"
 
 echo "menuentry \"Ubuntu 13.10 desktop amd64\" {
   set isofile=\"/iso/${IMAGE}\"
@@ -332,13 +314,13 @@ echo "menuentry \"Ubuntu 13.10 desktop amd64\" {
   initrd (loop)/casper/initrd.lz
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+  wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
 }
 
 function install_ubuntu13d_i386()
 {
-DL_ADDY="http://releases.ubuntu.com/13.10/"
-IMAGE="ubuntu-13.10-desktop-i386.iso"
+  DL_ADDY="http://releases.ubuntu.com/13.10/"
+  IMAGE="ubuntu-13.10-desktop-i386.iso"
 
 echo "menuentry \"Ubuntu 13.10 desktop i386\" {
   set isofile=\"/iso/${IMAGE}\"
@@ -348,7 +330,7 @@ echo "menuentry \"Ubuntu 13.10 desktop i386\" {
   initrd (loop)/casper/initrd.lz
 }
 " >> ${GRUBCONF}
-wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
+  wget ${DL_ADDY}${IMAGE}  --directory-prefix=${USBTMPDIR}/iso/
 }
 
 #### functions to run below this line ####
@@ -357,17 +339,16 @@ disk_detect
 disk_action
 disk_grub2
 install_grub_header
-# install_debian_amd64
-# install_fedora_amd64
-# install_fedora_i386
-# install_gentoo_amd64
-# install_gentoo_i386
-# install_kali 1.0.6 amd64
+install_debian_amd64
+install_fedora 20 x86_64 i386
+install_gentoo_amd64
+install_gentoo_i386
+install_kali 1.0.6 amd64
 install_netbsd 6.1.3 amd64 i386
 install_openbsd 5.4 amd64 i386
-# install_tails_i386
-# install_tails_i386_masq
-# install_ubuntu12s_amd64
-# install_ubuntu13d_amd64
-# install_ubuntu13d_i386
+install_tails_i386
+install_tails_i386_masq
+install_ubuntu12s_amd64
+install_ubuntu13d_amd64
+install_ubuntu13d_i386
 cleanup
