8ccd20618307726191142b94d78c08329bd1c5ae
cesar <cesar@poa.nyc>
2016-06-24 16:07:50 -0400

    legibility

--- freemem.sh
+++ freemem.sh
@@ -14,13 +14,13 @@ function precheck()
 {
     FM_PRE=$(echo $(\
         awk '/MemFree/ {print $2}' /proc/meminfo ) /1024.0 \
-            | ${BC})
+            | ${BC} )
     local CM_PRE=$(echo $(\
         awk '/^Cached/ {print $2}' /proc/meminfo ) /1024.0 \
-            | ${BC})
+            | ${BC} )
     local M_TOT=$(echo $(\
         awk '/MemTotal/ {print $2}' /proc/meminfo ) /1024.0 \
-            | ${BC})
+            | ${BC} )
     printf "%s\n" "" "This script clears cached mem and free's up ram." \
         "cached memory : ${CM_PRE}mb" \
         "free memory   : ${FM_PRE}mb" \
@@ -32,10 +32,10 @@ function check()
     sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
     local FM_POST=$(echo $(\
         awk '/MemFree/ {print $2}' /proc/meminfo ) /1024.0 \
-            | ${BC})
+            | ${BC} )
     printf "%s\n" "memory freed  : $(\
         echo "${FM_POST} - ${FM_PRE}" \
-            | ${BC})mb" \
+            | ${BC} )mb" \
         "total free    : ${FM_POST}mb" ""
 }
 
