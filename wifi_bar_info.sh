2275076e68322b17b35f12c9613dc8f5faa57bcf
cesar <cesar@pissedoffadmins.com>
2019-03-19 21:05:36 -0400

    changes: - initial commit

---
+++ wifi_bar_info.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#===============================================================================
+#          FILE: wifi_bar_info.sh
+#         USAGE: ./wifi_bar_info.sh
+#   DESCRIPTION: gives your essid and quality for use in tmux or xmobar
+#       OPTIONS: none
+#  REQUIREMENTS: wireless-tools, awk
+#          BUGS: probably a bunch
+#        AUTHOR: Cesar Bodden (), cesar@pissedoffadmins.com
+#  ORGANIZATION: pissedoffadmins.com
+#       CREATED: 03/19/2019 06:14:26 PM EDT
+#      REVISION: .1
+#===============================================================================
+
+set -o nounset
+
+_IFACE=$(\
+    iwconfig 2>&1 \
+    | awk '/IEEE/ {print $1}')
+
+_ESSID=$(\
+    iwconfig ${_IFACE} 2>&1 \
+    | awk -F '"' '/ESSID/ {print $2}')
+
+if [[ -z ${_ESSID} ]]
+then
+    printf "%s\n" "No wifi detected"
+    exit 0
+fi
+
+_STR=$(\
+    iwconfig ${_IFACE} 2>&1 \
+    | awk -F '[ |=]' '/Quality/ {print $13}')
+
+_BAR=$(\
+    expr ${_STR%%/*} / 10)
+
+_TIC=$(eval \
+    printf "X%.0s" {1..${_BAR}})
+
+_MRK=$(eval \
+    printf "%s%-$((${_STR##*/}/10))s%s" "[" "${_TIC}" "]" \
+    | tr ' ~' '- ')
+
+echo ${_ESSID} ${_MRK}
