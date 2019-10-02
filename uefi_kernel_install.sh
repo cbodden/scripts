c5bba900047e20f90dacc530eef8639656e0d718
cesar <cesar@pissedoffadmins.com>
2019-10-02 09:59:14 -0400

    changes: - making it easier to read and follow

--- uefi_kernel_install.sh
+++ uefi_kernel_install.sh
@@ -17,127 +17,128 @@
 
 LC_ALL=C
 LANG=C
+set -e
 set -o nounset
 set -o pipefail
+set -u
 trap 'echo "${NAME}: Ouch! Quitting." 1>&2 ; exit 1' 1 2 3 9 15
 
 function main()
 {
-    NAME=$(basename $0)
-    BZIMG="/arch/x86/boot/bzImage"
-    BOOT="/boot/EFI/gentoo/"
-    KERN_PATH=$(readlink -f /usr/src/linux)
-    KERN_VER=$(basename ${KERN_PATH})
-    KERN_VER_FULL=$(file ${KERN_PATH}${BZIMG} \
-        | awk '{print $9}')
-    _DISKMAJ=$(sudo lsblk -a -p -l \
-        | awk '/\ \/boot/')
-    _DISK=$(echo ${_DISKMAJ} \
-        | awk '{print $1}')
-    _MAJ=$(echo ${_DISKMAJ} \
-        | awk '{print $2}' \
-        | cut -d: -f2)
-    _RED=$(tput setaf 1)
-    _BLU=$(tput setaf 4)
-    _GRN=$(tput setaf 40)
-    _CLR=$(tput sgr0)
+    readonly NAME=$(basename $0)
+    readonly BZIMG="/arch/x86/boot/bzImage"
+    readonly BOOT="/boot/EFI/gentoo/"
+    readonly KERN_PATH=$(readlink -f /usr/src/linux)
+    readonly KERN_VER=$(basename ${KERN_PATH})
+    readonly KERN_VER_FULL=$(file ${KERN_PATH}${BZIMG} \
+                 | awk '{print $9}')
+    readonly DISKMAJ=$(sudo lsblk -a -p -l \
+                 | awk '/\ \/boot/')
+    readonly DISK=$(echo ${DISKMAJ} \
+                 | awk '{print $1}')
+    readonly MAJ=$(echo ${DISKMAJ} \
+                 | awk '{print $2}' \
+                 | cut -d: -f2)
+    readonly RED=$(tput setaf 1)
+    readonly BLU=$(tput setaf 4)
+    readonly GRN=$(tput setaf 40)
+    readonly CLR=$(tput sgr0)
     clear
 }
 
-function pause()
+function _Pause()
 {
     printf "%s\n" \
-        "${_GRN}. . . .Press enter to continue. . . .${_CLR}"
+        "${GRN}. . . .Press enter to continue. . . .${CLR}"
     read -p "$*"
 }
 
-function _testVars()
+function _TestVars()
 {
     clear
     printf "%s\n" \
-        "${_BLU}Kernel Path          : ${_RED}${KERN_PATH}" \
-        "${_BLU}Kernel Version       : ${_RED}${KERN_VER}" \
-        "${_BLU}Kernel Version Full  : ${_RED}${KERN_VER_FULL}" \
-        "${_CLR}"
-    pause
+        "${BLU}Kernel Path          : ${RED}${KERN_PATH}" \
+        "${BLU}Kernel Version       : ${RED}${KERN_VER}" \
+        "${BLU}Kernel Version Full  : ${RED}${KERN_VER_FULL}" \
+        "${CLR}"
+    _Pause
 }
 
-function _rw_efivars()
+function _RW_efivars()
 {
     printf "%s\n" \
-        "${_BLU}Now mounting /sys/firmware/efi/efivars read write" \
-        "${_RED}sudo mount /sys/firmware/efi/efivars -o rw,remount" \
-        "${_CLR}"
-    pause
+        "${BLU}Now mounting /sys/firmware/efi/efivars read write" \
+        "${RED}sudo mount /sys/firmware/efi/efivars -o rw,remount" \
+        "${CLR}"
+    _Pause
     sudo mount /sys/firmware/efi/efivars -o rw,remount
 }
 
-function _kernel_to_boot()
+function _Kernel_to_Boot()
 {
     printf "%s\n" \
-        "${_BLU}Now copying ${KERN_VER_FULL} to ${BOOT}" \
-        "${_RED}cp ${KERN_PATH}${BZIMG} ${BOOT}bzImage-${KERN_VER_FULL}.efi" \
-        "${_CLR}"
-    pause
+        "${BLU}Now copying ${KERN_VER_FULL} to ${BOOT}" \
+        "${RED}cp ${KERN_PATH}${BZIMG} ${BOOT}bzImage-${KERN_VER_FULL}.efi" \
+        "${CLR}"
+    _Pause
     sudo cp ${KERN_PATH}${BZIMG} ${BOOT}bzImage-${KERN_VER_FULL}.efi
 }
 
-function _make_initramfs()
+function _Make_initramfs()
 {
     printf "%s\n" \
-        "${_BLU}Now generating ${BOOT}initramfs-${KERN_VER_FULL}.img" \
-        "${_RED}sudo dracut ${BOOT}initramfs-${KERN_VER_FULL}.img" \
-        "${_CLR}"
-    pause
+        "${BLU}Now generating ${BOOT}initramfs-${KERN_VER_FULL}.img" \
+        "${RED}sudo dracut ${BOOT}initramfs-${KERN_VER_FULL}.img" \
+        "${CLR}"
+    _Pause
     sudo dracut ${BOOT}initramfs-${KERN_VER_FULL}.img
 }
 
-function _clear_old_boot
+function _Clear_Old_Boot
 {
-        ## | awk '/^Boot0/ {print substr($1, 0, length($1)-1)}')
     local _UEFI_OBJ=$(sudo efibootmgr \
         | awk '/[Gg]entoo/ {print substr($1, 0, length($1)-1)}')
     printf "%s\n" \
-        "${_RED}Now clearing the default boot from efibootmgr" \
-        "${_CLR}"
-    pause
+        "${RED}Now clearing the default boot from efibootmgr" \
+        "${CLR}"
+    _Pause
     for ITER in ${_UEFI_OBJ}
     do
         sudo efibootmgr -b ${ITER#Boot} -B
     done
 }
 
-function _install_new_boot
+function _Install_New_Boot
 {
-    _P0="/usr/sbin/efibootmgr"
-    _P1=" -c -d ${_DISK}"
-    _P2=" -p ${_MAJ} -L \"Gentoo ${KERN_VER_FULL}\""
-    _P3=" -l '\EFI\gentoo\bzImage-${KERN_VER_FULL}.efi'"
-    _P4=" -u 'initrd=\EFI\gentoo\initramfs-${KERN_VER_FULL}.img'"
+    local _P0="/usr/sbin/efibootmgr"
+    local _P1=" -c -d ${DISK}"
+    local _P2=" -p ${MAJ} -L \"Gentoo ${KERN_VER_FULL}\""
+    local _P3=" -l '\EFI\gentoo\bzImage-${KERN_VER_FULL}.efi'"
+    local _P4=" -u 'initrd=\EFI\gentoo\initramfs-${KERN_VER_FULL}.img'"
     printf "%s\n" \
-        "${_BLU}Now installing the new kernel and initramfs to UEFI" \
-        "${_RED}${_P0}${_P1}${_P2}${_P3}${_P4}" \
-        "${_CLR}"
-    pause
+        "${BLU}Now installing the new kernel and initramfs to UEFI" \
+        "${RED}${_P0}${_P1}${_P2}${_P3}${_P4}" \
+        "${CLR}"
+    _Pause
     eval sudo ${_P0}${_P1}${_P2}${_P3}${_P4}
 }
 
-function _ro_efivars()
+function _RO_efivars()
 {
     printf "%s\n" \
-        "${_BLU}Now mounting /sys/firmware/efi/efivars read only" \
-        "${_RED}sudo mount /sys/firmware/efi/efivars -o ro,remount" \
-        "${_CLR}"
-    pause
+        "${BLU}Now mounting /sys/firmware/efi/efivars read only" \
+        "${RED}sudo mount /sys/firmware/efi/efivars -o ro,remount" \
+        "${CLR}"
+    _Pause
     sudo mount /sys/firmware/efi/efivars -o ro,remount
 }
 
 main
-pause
-_testVars
-_rw_efivars
-_kernel_to_boot
-_make_initramfs
-_clear_old_boot
-_install_new_boot
-_ro_efivars
+_Pause
+_TestVars
+_RW_efivars
+_Kernel_to_Boot
+_Make_initramfs
+_Clear_Old_Boot
+_Install_New_Boot
+_RO_efivars
