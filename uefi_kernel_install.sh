f141d6d95478ba693e67cbaeea70bf0bcd124993
cesar <cesar@pissedoffadmins.com>
2019-10-02 10:51:41 -0400

    changes: - removed redundant sudo calls - added dependency check - added sudo check

--- uefi_kernel_install.sh
+++ uefi_kernel_install.sh
@@ -6,7 +6,7 @@
 #
 #   DESCRIPTION: this script assists with adding a new kernel to efi stub
 #       OPTIONS: none
-#  REQUIREMENTS: efibootmgr, sudo, dracut, lsblk
+#  REQUIREMENTS: efibootmgr, dracut, lsblk
 #          BUGS: none so far
 #         NOTES: use wisely
 #        AUTHOR: Cesar Bodden (), cesar@pissedoffadmins.com
@@ -25,6 +25,30 @@ trap 'echo "${NAME}: Ouch! Quitting." 1>&2 ; exit 1' 1 2 3 9 15
 
 function main()
 {
+    readonly RED=$(tput setaf 1)
+    readonly BLU=$(tput setaf 4)
+    readonly GRN=$(tput setaf 40)
+    readonly CLR=$(tput sgr0)
+    local _R_UID="0"
+    if [ "${UID}" -ne "${_R_UID}" ]
+    then
+        printf "%s\n" \
+            "${RED}. . .Needs sudo. . .${CLR}"
+        exit 1
+    fi
+    local _DEPS="mount dracut efibootmgr lsblk"
+    for ITER in ${_DEPS}
+    do
+        if [ -z "$(which ${ITER} 2>/dev/null)" ]
+        then
+            printf "%s\n" \
+                "${RED}. . .${ITER} not found. . .${CLR}"
+            exit 1
+        else
+            readonly ${ITER^^}="$(which ${ITER})"
+        fi
+    done
+
     readonly NAME=$(basename $0)
     readonly BZIMG="/arch/x86/boot/bzImage"
     readonly BOOT="/boot/EFI/gentoo/"
@@ -32,18 +56,14 @@ function main()
     readonly KERN_VER=$(basename ${KERN_PATH})
     readonly KERN_VER_FULL=$(file ${KERN_PATH}${BZIMG} \
                  | awk '{print $9}')
-    readonly DISKMAJ=$(sudo lsblk -a -p -l \
+    readonly DISKMAJ=$(${LSBLK} -a -p -l \
                  | awk '/\ \/boot/')
     readonly DISK=$(echo ${DISKMAJ} \
                  | awk '{print $1}')
     readonly MAJ=$(echo ${DISKMAJ} \
                  | awk '{print $2}' \
                  | cut -d: -f2)
-    readonly RED=$(tput setaf 1)
-    readonly BLU=$(tput setaf 4)
-    readonly GRN=$(tput setaf 40)
-    readonly CLR=$(tput sgr0)
-    clear
+   clear
 }
 
 function _Pause()
@@ -68,10 +88,10 @@ function _RW_efivars()
 {
     printf "%s\n" \
         "${BLU}Now mounting /sys/firmware/efi/efivars read write" \
-        "${RED}sudo mount /sys/firmware/efi/efivars -o rw,remount" \
+        "${RED}${MOUNT} /sys/firmware/efi/efivars -o rw,remount" \
         "${CLR}"
     _Pause
-    sudo mount /sys/firmware/efi/efivars -o rw,remount
+    ${MOUNT} /sys/firmware/efi/efivars -o rw,remount
 }
 
 function _Kernel_to_Boot()
@@ -81,22 +101,22 @@ function _Kernel_to_Boot()
         "${RED}cp ${KERN_PATH}${BZIMG} ${BOOT}bzImage-${KERN_VER_FULL}.efi" \
         "${CLR}"
     _Pause
-    sudo cp ${KERN_PATH}${BZIMG} ${BOOT}bzImage-${KERN_VER_FULL}.efi
+    cp ${KERN_PATH}${BZIMG} ${BOOT}bzImage-${KERN_VER_FULL}.efi
 }
 
 function _Make_initramfs()
 {
     printf "%s\n" \
         "${BLU}Now generating ${BOOT}initramfs-${KERN_VER_FULL}.img" \
-        "${RED}sudo dracut ${BOOT}initramfs-${KERN_VER_FULL}.img" \
+        "${RED}${DRACUT} ${BOOT}initramfs-${KERN_VER_FULL}.img" \
         "${CLR}"
     _Pause
-    sudo dracut ${BOOT}initramfs-${KERN_VER_FULL}.img
+    ${DRACUT} ${BOOT}initramfs-${KERN_VER_FULL}.img
 }
 
 function _Clear_Old_Boot
 {
-    local _UEFI_OBJ=$(sudo efibootmgr \
+    local _UEFI_OBJ=$(${EFIBOOTMGR} \
         | awk '/[Gg]entoo/ {print substr($1, 0, length($1)-1)}')
     printf "%s\n" \
         "${RED}Now clearing the default boot from efibootmgr" \
@@ -104,13 +124,13 @@ function _Clear_Old_Boot
     _Pause
     for ITER in ${_UEFI_OBJ}
     do
-        sudo efibootmgr -b ${ITER#Boot} -B
+        ${EFIBOOTMGR} -b ${ITER#Boot} -B
     done
 }
 
 function _Install_New_Boot
 {
-    local _P0="/usr/sbin/efibootmgr"
+    local _P0="${EFIBOOTMGR}"
     local _P1=" -c -d ${DISK}"
     local _P2=" -p ${MAJ} -L \"Gentoo ${KERN_VER_FULL}\""
     local _P3=" -l '\EFI\gentoo\bzImage-${KERN_VER_FULL}.efi'"
@@ -120,17 +140,17 @@ function _Install_New_Boot
         "${RED}${_P0}${_P1}${_P2}${_P3}${_P4}" \
         "${CLR}"
     _Pause
-    eval sudo ${_P0}${_P1}${_P2}${_P3}${_P4}
+    eval ${_P0}${_P1}${_P2}${_P3}${_P4}
 }
 
 function _RO_efivars()
 {
     printf "%s\n" \
         "${BLU}Now mounting /sys/firmware/efi/efivars read only" \
-        "${RED}sudo mount /sys/firmware/efi/efivars -o ro,remount" \
+        "${RED}${MOUNT} /sys/firmware/efi/efivars -o ro,remount" \
         "${CLR}"
     _Pause
-    sudo mount /sys/firmware/efi/efivars -o ro,remount
+    ${MOUNT} /sys/firmware/efi/efivars -o ro,remount
 }
 
 main
