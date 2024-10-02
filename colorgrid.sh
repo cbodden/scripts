68b7b6756c2370de7f6d9e9836c6bb9692379f7e
cesar b <cesar@poa.nyc>
2024-10-01 22:53:12 -0400

    added: - colorgrid

--- colorgrid.sh
+++ colorgrid.sh
@@ -1,50 +1,49 @@
-#!/bin/bash
-# http://www.linuxgazette.net/issue65/padala.html
+#!/usr/bin/env bash
 
-function cgrid() {
-    printf "\n"
-    for x in $(seq -w 029 047); do
-        echo -n ${x}\;XXX "|"
-        for y in $(seq -w 029 047); do
-            if [ "$x" == "029" ]; then
-                echo -en "\033[4m" XXX\;${y} "\033[0m"
-            else
-                echo -en "\033[0;${x};${y}m" ${x}\;${y} "\033[0m"
-            fi
+LC_ALL=C
+LANG=C
+set -e
+set -o nounset
+set -o pipefail
+set -u
+
+printf "\n" ""
+
+for ITER in {16..51}
+do
+    NUM=${ITER##0}
+    tput setaf ${ITER##0}
+    tput setab ${ITER##0}
+    printf "%3s" "${ITER}"
+
+    if [ $((NUM % 3)) -eq 0 ]
+    then
+        tput sgr0
+        printf "%3s" ""
+
+        for RUN in {34,70,106,142,178}
+        do
+            NUM_MOD=$((${NUM} + ${RUN}))
+            for ITER_2 in {1..3}
+            do
+                tput setaf ${NUM_MOD##0}
+                tput setab ${NUM_MOD##0}
+                printf "%3s" "${NUM_MOD}"
+                NUM_MOD=$((${NUM_MOD} + 1))
+
+                if [ ${ITER_2} -eq 3 ]
+                then
+                    tput sgr0
+                    if [ ${RUN} -eq 178 ]
+                    then
+                        printf "%3s\n" ""
+                    else
+                        printf "%3s" ""
+                    fi
+                fi
+            done
         done
-        printf "\n"
-    done
-}
+    fi
+done
 
-function pgrid() {
-    printf "\n"
-    for x in $(seq -w 089 107); do
-        echo -n ${x}\;XXX "|"
-        for y in $(seq -w 089 107); do
-            if [ "$x" == "089" ]; then
-                echo -en "\033[4m" XXX\;${y} "\033[0m"
-            else
-                echo -en "\033[0;${x};${y}m" ${x}\;${y} "\033[0m"
-            fi
-        done
-        printf "\n"
-    done
-}
-
-case $1 in
-    'normal'|'regular'|'standard')
-        cgrid
-    ;;
-
-    'pastel'|'pastels')
-        pgrid
-    ;;
-
-    'all')
-        cgrid
-        pgrid
-        printf "\n"
-    ;;
-
-'help'|*) echo "usage: $0 <normal|pastel|all>"
-esac
+printf "\n" ""
